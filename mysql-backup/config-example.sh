16de98996e54dac23c4d591d21f29c5bbc628fd2
André Luiz Girol <andregirol@gmail.com>
2017-08-04 18:21:04 -0300

    Added email configuration

--- mysql-backup/config-example.sh
+++ mysql-backup/config-example.sh
@@ -1,19 +1,38 @@
-# Configurações de banco de dados
+# Database configuration
 database="my_database"
 dbhost="localhost"
 dbuser="userdb"
 dbpass="mypass"
 
-# Dados do servidor de arquivos
-# Usuário
-sshuser="my_ssh_user"
+# ==========================
+# Mail Server configuration
+# ==========================
 
-# hostname do servidor de arquivos
+# User Configuration
+from='the_sender@domain.com'
+to='the_destination@domain.com'
+subject="My Subject Here"
+message="Write what you want"
+
+# Settings for Gmail.
+# For other services, check sendEmail documentation
+
+# Usage: hostname.com:[port]
+smtp='smtp.gmail.com:587'
+
+# Gmail username WITHOUT @gmail.com
+username='my_gmail_user'
+pass='my_password'
+
+# ==========================
+# File Server Configuration
+# ==========================
+
+sshuser="my_ssh_user"
 fileserver="my.host.com"
 
-# pasta de destino
-# manter a barra ao lado direito!!
+# Destination folder WITH TRAILING SLASH
 serverfolder="backup/destination/"
 
-# URL completa:
+# Assembles ssh URI
 destination="$sshuser@$fileserver:$serverfolder"
