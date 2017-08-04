502ce40badb1b32861a284d62bc274379e5f8e23
André Luiz Girol <andregirol@gmail.com>
2017-08-04 19:02:49 -0300

    Changed to Backup to Email

--- mysql-backup/make-backup.sh
+++ mysql-backup/make-backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Script para realização de backup da base de dados MySQL
 # Data de criação: 27/07/2015
 # Dev: André Girol - andregirol at gmail dot com
@@ -6,29 +7,36 @@
 # Inclui Configuração inicial
 source config.sh
 
-
+# Formata data para o arquivo SQL
 # Output ->Database + Data
-data="_`date +%Y_%m_%d__%H-%M-%S`"
-# Concatenando
-arquivo=$database$data
+file_date="_`date +%Y_%m_%d__%H-%M-%S`"
 
-echo $arquivo
+# Concatenando
+dump_file=$database$file_date
+
+#echo $dump_file
 
 echo "Iniciando dump para $database em $dbhost..."
-mysqldump -c -h $dbhost --user $dbuser --password=$dbpass $database > $arquivo.sql
-
+mysqldump -c -h $dbhost --user $dbuser --password=$dbpass $database > $dump_file.sql
 
 # Acertando variáveis
-localfile="$arquivo.sql"
+the_backup="$dump_file.sql"
 
-destfile="$destination$arquivo.sql"
+echo "Sending Backup to Email..."
+sendEmail -f "$from" -t $to -u "$subject" -m "$message" -a $the_backup -s $smtp -o tls=yes -xu $username -xp $pass
 
-echo "Arquivo de destino é: $destfile"
+# ============
 
-echo "Fazendo upload para o servidor..."
-scp $localfile $destfile
+# Here notes for using a file server and backup over ssh
 
-echo "Backup realizado com sucesso!"
+#destfile="$destination$arquivo.sql"
+
+#echo "Arquivo de destino é: $destfile"
+
+#echo "Fazendo upload para o servidor..."
+#scp $localfile $destfile
+
+#echo "Backup realizado com sucesso!"
 
 
 #cat $arquivo.sql
